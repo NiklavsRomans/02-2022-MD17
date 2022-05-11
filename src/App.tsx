0b3511ca7cbd105b3f509389d01a2ab5110af85b
Niklavs Romans <niklavsromans19@gmail.com>
2022-05-12 00:18:10 +0200

    default files

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import HomePage from './pages/homepage/HomePage';
+import Characters from './pages/characters/Characters';
+import Character from './pages/character/Character';
+import Episodes from './pages/episodes/Episodes';
+import Episode from './pages/episode/Episode';
+import Locations from './pages/locations/Locations';
+import LocationPage from './pages/location/Location';
+import Page404 from './pages/page404/Page404';
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/characters/:id" element={<Character />} />
+        <Route path="/episode" element={<Episodes />} />
+        <Route path="/episode/:id" element={<Episode />} />
+        <Route path="/location" element={<Locations />} />
+        <Route path="/location/:id" element={<LocationPage />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </Router>
   </div>
 );
 
