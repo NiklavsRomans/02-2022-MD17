0188810121477140d50931fe096c6d7cca37cd73
Niklavs Romans <niklavsromans19@gmail.com>
2022-05-12 18:49:05 +0200

    removed app.scss import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import HomePage from './pages/homepage/HomePage';
