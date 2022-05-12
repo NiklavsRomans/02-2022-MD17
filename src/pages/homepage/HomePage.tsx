7566944c17a3d1a6fd189b3824bd901af8bba79b
Niklavs Romans <niklavsromans19@gmail.com>
2022-05-12 19:09:11 +0200

    fixed

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -8,7 +8,13 @@ const HomePage = () => {
       <div className="row">
         <div className="col-xs-12">
           <div className="home-content">
-            <h2>Nobody exists on purpose, nobody belongs anywhere, everybodys gonna die; Come watch TV</h2>
+            <h2>
+              Nobody exists on purpose, nobody belongs anywhere, everybodys gonna die;
+              {' '}
+              <br />
+              {' '}
+              Come watch TV
+            </h2>
           </div>
         </div>
       </div>
